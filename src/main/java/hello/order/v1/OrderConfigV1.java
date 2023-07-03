4e1835c6e563ec150ad446c993500fecfbc8e4c1
tlsrhksgh <sin99145@naver.com>
2023-07-03 11:58:44 +0900

    모니터링 메트릭 커스텀(V1 CounterBuilder, V2 AOP)

---
+++ src/main/java/hello/order/v1/OrderConfigV1.java
@@ -0,0 +1,16 @@
+package hello.order.v1;
+
+import hello.order.OrderService;
+import hello.order.v0.OrderServiceV0;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV1 {
+
+    @Bean
+    OrderService orderService(MeterRegistry meterRegistry) {
+        return new OrderServiceV1(meterRegistry);
+    }
+}
